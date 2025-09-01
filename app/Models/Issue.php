330777d4e71915ba270f2889f80e1948181dcf1b
Argjend Kurteshi <142898279+Gjentii@users.noreply.github.com>
2025-09-01 20:51:48 +0200

    Relation implemented
    - Relation many-to-one between issues and projects

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Issue extends Model
 {
@@ -14,4 +15,9 @@ class Issue extends Model
         'priority',
         'due_date',
     ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -12,4 +13,9 @@ class Project extends Model
         'start_date',
         'deadline',
     ];
+
+    public function issues(): HasMany
+    {
+        return $this->hasMany(Issue::class);
+    }
 }
