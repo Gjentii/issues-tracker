9edd3543f59a1d10c814e39eee23edfaa842ac12
Argjend Kurteshi <142898279+Gjentii@users.noreply.github.com>
2025-09-01 20:50:05 +0200

    Issues updates
    - Migration and Model implemented

---
+++ app/Models/Issue.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Issue extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date',
+    ];
+}
