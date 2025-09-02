f184de41f1cc5cdd2f86fb92969ce97fc6ff1c9e
Argjend Kurteshi <142898279+Gjentii@users.noreply.github.com>
2025-09-02 04:54:00 +0200

    Many to one relation
    - Many to one relation between comments and issues implemented

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Issue extends Model
 {
@@ -25,5 +26,10 @@ class Issue extends Model
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class, 'issues_tags');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
